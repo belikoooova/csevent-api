2c07202eb80c4aae8caeb94cf84426d535efc6c5
Maria Belikova <mebelikova@edu.hse.ru>
2024-03-20 22:14:33 +0300

    Initial commit

---
+++ src/main/java/com/example/cseventapi/CseventApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cseventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseventApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CseventApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cseventapi/CseventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cseventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CseventApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
